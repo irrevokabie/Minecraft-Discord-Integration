81f81987df32a20d55e2f8ad11ab102b66e9cf23
irrevokable <irrevokable@is.notaskid.ong>
2026-02-22 18:44:05 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+import requests
+import time
+import re
+
+webhook_url = "" # !
+log_path = "logs/latest.log"
+chat_regex = re.compile(r"<(.+?)> (.+)")
+lines_read = set()
+
+def send_webhook(username, message):
+    embed = {
+        "title": username,
+        "description": f"`{message}`",
+        "color": 3066993
+    }
+
+    response = requests.post(webhook_url, json={"embeds": [embed]})
+    if response.status_code != 204:
+        print(f"{response.status_code} : {response.text}")
+
+def mc_log():
+    with open(log_path, "r", encoding="utf-8") as file:
+        file.seek(0, 2)
+        while True:
+            line = file.readline()
+            if not line:
+                time.sleep(0.1)
+                continue
+            if line in lines_read:
+                continue
+            lines_read.add(line)
+            match = chat_regex.search(line)
+            if match:
+                username, message = match.groups()
+                send_webhook(username, message)
+
+if __name__ == "__main__":
+    mc_log()
